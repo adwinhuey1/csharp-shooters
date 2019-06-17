5231b63140f7584b520c85faf6617de5c2b872b0
alexis-acosta <44756330+alexis-acosta@users.noreply.github.com>
2019-06-16 17:07:27 -0700

    MVC commit with database

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace valkyrieIMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
