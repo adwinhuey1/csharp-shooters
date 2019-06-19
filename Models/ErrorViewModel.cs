962455f2f100371f6b92635912a288e775fd90c4
Nathan Brenke <nbrenke@gmail.com>
2019-06-18 22:07:01 -0400

    Controller
    Controller Created and Routes

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ValkyrieIMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
